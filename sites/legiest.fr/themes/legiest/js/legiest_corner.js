13bc12333313991b3a2e4a8f46531334483d5f07
Christophe Espiau <christophe.espiau@korora.fr>
2012-07-23 12:33:20 +0200

    up theme legiest : front page and news displays theming

---
+++ sites/legiest.fr/themes/legiest/js/legiest_corner.js
@@ -0,0 +1,16 @@
+/**
+ * Jquery Corner implementation for Legiest website,
+ * following instructions at http://drupal.org/node/1030144
+ */
+(function ($) {
+  Drupal.behaviors.legiest = {
+    attach: function(context, settings) {
+      // list every element that you would like to adorn with corners below
+      $("#block-search-form").corner("tl tr bl br 5px");
+      $("#region-sidebar-first").corner("tl tr bl br 5px");
+      $("#region-content").corner("tl tr bl br 5px");
+      $("#views_slideshow_cycle_main_news-block .views-field-field-picture img").corner("bl br 10px");
+      $("#views_slideshow_cycle_main_news-block .views-field-body").corner("bl br 10px");
+    }
+  }
+})(jQuery);
