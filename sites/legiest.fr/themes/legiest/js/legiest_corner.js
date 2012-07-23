7cbe4ba83a6d8c555e2879398b818be9ccf0995a
Christophe Espiau <christophe.espiau@korora.fr>
2012-07-23 15:52:04 +0200

    up legiest theme : sidebar menu and Contact box modifications

--- sites/legiest.fr/themes/legiest/js/legiest_corner.js
+++ sites/legiest.fr/themes/legiest/js/legiest_corner.js
@@ -7,7 +7,9 @@
     attach: function(context, settings) {
       // list every element that you would like to adorn with corners below
       $("#block-search-form").corner("tl tr bl br 5px");
-      $("#region-sidebar-first").corner("tl tr bl br 5px");
+      $("#block-views-categories-block-all").corner("tl tr 5px");
+      $("#block-boxes-contact-legiest").corner("bl br 5px");
+      $("#block-views-news-block-1").corner("tl tr bl br 5px");
       $("#region-content").corner("tl tr bl br 5px");
       $("#views_slideshow_cycle_main_news-block .views-field-field-picture img").corner("bl br 10px");
       $("#views_slideshow_cycle_main_news-block .views-field-body").corner("bl br 10px");
